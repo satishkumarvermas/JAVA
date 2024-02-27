e143f47209c7e7522e6840e7c6c7dfabeb0f9817
Satish Kumar Verma <142597434+satishkumarvermas@users.noreply.github.com>
2024-02-27 21:01:19 +0530

    Add files via upload

---
+++ LastToFirst.java
@@ -0,0 +1,13 @@
+public class LastToFirst 
+{
+    public static void main(String[] args)   
+	{  
+		int last=5;  
+		while(last>=0)   
+		{  
+			System.out.print(last);  
+			last=last-1;  
+		}  
+		
+	}  
+}
