e7f266fea4952eeadc2c12b499365b24dc73c862
Satish Kumar Verma <142597434+satishkumarvermas@users.noreply.github.com>
2024-02-27 21:02:14 +0530

    Add files via upload

---
+++ Digitcount.java
@@ -0,0 +1,14 @@
+public class Digitcount   
+{  
+	public static void main(String[] args)   
+	{  
+		int n=1234;
+		int count=0;
+		while(n>0)
+		{
+			n=n/10;
+			count++;
+		}
+		System.out.println("The digit count is:"+ count);  
+	}  
+}
---
+++ FirstToLast.java
@@ -0,0 +1,14 @@
+public class FirstToLast   
+{  
+	public static void main(String[] args)   
+	{  
+		int first=1;
+		int last=5;		
+		while(first<=last)   
+		{  
+			System.out.print(first);  
+			first=first+1;  
+		}  
+		
+	}  
+}
